89773d5780bcf5272c10b194dc6199b2f7764a55
supriyaaddla <35639874+supriyaaddla@users.noreply.github.com>
2024-09-25 00:26:05 -0500

    update healthcheck

---
+++ DataAPI/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using DataAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAPI.Controllers;
+
+[ApiController]
+public class HomeController : ControllerBase
+{
+
+    private readonly MyDbContext _context;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger, MyDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    [Route("/")]
+    public string Get()
+    {
+        return "OK";
+    }
+}
+
